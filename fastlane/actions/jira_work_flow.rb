7040b99475531bee15dd1e44ae2652eadc5716c8
i.kvyatkovskiy <i.kvyatkovskiy@rambler-co.ru>
2017-09-28 17:44:23 +0300

    Disabled jira workflow

--- fastlane/actions/jira_work_flow.rb
+++ fastlane/actions/jira_work_flow.rb
@@ -1,57 +1,57 @@
-# require 'jira-ruby'
+require 'jira-ruby'
 
 module Fastlane
   module Actions
     class JiraWorkFlowAction < Action
       
-      # def self.run(params)
+      def self.run(params)
 
-      #   # Authorization
-      #   options = {
-      #     :username     => params[:jira_username],
-      #     :password     => params[:jira_password],
-      #     :site         => params[:jira_host],
-      #     :context_path => '',
-      #     :auth_type    => :basic
-      #   }
-      #   @client = JIRA::Client.new(options)
+        # Authorization
+        options = {
+          :username     => params[:jira_username],
+          :password     => params[:jira_password],
+          :site         => params[:jira_host],
+          :context_path => '',
+          :auth_type    => :basic
+        }
+        @client = JIRA::Client.new(options)
 
-      #   puts "Jira issues #{params[:issue_ids]}"
+        puts "Jira issues #{params[:issue_ids]}"
 
-      #   # Move
-      #   issues = params[:issue_ids].uniq
-      #   issues.each do |issue_id|
-      #     move_issue(issue_id, params[:jira_transition_name])
-      #   end
+        # Move
+        issues = params[:issue_ids].uniq
+        issues.each do |issue_id|
+          move_issue(issue_id, params[:jira_transition_name])
+        end
 
-      # end
+      end
 
-      # def  self.move_issue(issue_id, transition_name)
+      def  self.move_issue(issue_id, transition_name)
 
-      #   # Find Issue
-      #   issue = @client.Issue.find(issue_id)
-      #   if !issue
-      #     puts "Issue #{issue_id} not found"
-      #     return
-      #   end
+        # Find Issue
+        issue = @client.Issue.find(issue_id)
+        if !issue
+          puts "Issue #{issue_id} not found"
+          return
+        end
         
-      #   if issue.status.name == 'Resolved' || issue.status.name == 'Closed'
-      #     puts "Issues #{issue_id} is #{issue.status.name}"
-      #     return
-      #   end
+        if issue.status.name == 'Resolved' || issue.status.name == 'Closed'
+          puts "Issues #{issue_id} is #{issue.status.name}"
+          return
+        end
 
-      #   # Find Transition
-      #   transitions = @client.Transition.all(issue: issue)
-      #   transition = transitions.find{ |elem| elem.name == transition_name }
-      #   if !transition
-      #     puts "Can't move issue #{issue_id} to #{transition_name}"
-      #     return
-      #   end
+        # Find Transition
+        transitions = @client.Transition.all(issue: issue)
+        transition = transitions.find{ |elem| elem.name == transition_name }
+        if !transition
+          puts "Can't move issue #{issue_id} to #{transition_name}"
+          return
+        end
 
-      #   # Perform Transition
-      #   new_transition = issue.transitions.build
-      #   new_transition.save!("transition" => {"id" => transition.id})
-      #   puts "Move issue #{issue_id} to #{transition_name}"
+        # Perform Transition
+        new_transition = issue.transitions.build
+        new_transition.save!("transition" => {"id" => transition.id})
+        puts "Move issue #{issue_id} to #{transition_name}"
 
       end
 
